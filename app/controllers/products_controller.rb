5237636c4e5d4391d46e7215cc785364ce45c926
Stephen A. Crump <sacrump@gmail.com>
2014-02-11 14:17:49 -0600

    Week 5 HW Complete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
 
   def index
-    @product_list = []
+    @product_list = Product.all
   end
 
   def show
-    @product = "iPhone"
+    @product_list = Product.all
   end
 
 end
